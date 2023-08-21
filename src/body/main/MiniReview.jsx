a2eb99a412fb695579388964ca7afecb2966468d
Hemanth kumar <106808387+Hemu21@users.noreply.github.com>
2023-08-22 02:41:28 +0530

    Update MiniReview.jsx

--- src/body/main/MiniReview.jsx
+++ src/body/main/MiniReview.jsx
@@ -6,7 +6,7 @@ export default function MiniReview(props) {
   return (
     <div className='desc' style={{border:"0.2px wheat solid",borderRadius:"5px"}}>
         <h4>Rating:</h4>
-        <h2 style={{marginLeft:"47.5%"}} ><ReactStars value={props.rating} count={5} edit={false} color2={'#ffd700'} /></h2>
+        <h2 style={{marginLeft:"47%"}} ><ReactStars value={props.rating} count={5} edit={false} color2={'#ffd700'} /></h2>
         <h4>Description: {props.desc}</h4>
     </div>
   )
